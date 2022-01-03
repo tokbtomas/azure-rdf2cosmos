6f382f6ba49511077f9f4a308f311422fc83a369
Chris Joakim <christopher.joakim@gmail.com>
2022-01-03 12:08:33 -0500

    dec PowerShell scripts

--- rdf2cosmos/dec_3_convert_objects_to_gremlin.ps1
+++ rdf2cosmos/dec_3_convert_objects_to_gremlin.ps1
@@ -3,25 +3,16 @@
 # "Groovy"\Gremlin for loading the data into CosmosDB.
 # Chris Joakim, Microsoft, January 2022
 
-ddir=$AZURE_RDF2COSMOS_DATA_DIR  # ddir is a shorthand abbreviation for AZURE_RDF2COSMOS_DATA_DIR
+$groovy_outfile = Join-Path $env:AZURE_RDF2COSMOS_DATA_DIR '\gremlin\groovy.txt'
+$log_outfile="log\convert_objects_to_gremlin.txt"
 
-log_outfile="log\convert_objects_to_gremlin.txt"
-
-echo 'removing groovy.txt output file'
-rm $ddir\gremlin\groovy.txt
+if (Test-Path $groovy_outfile) {
+    Remove-Item $groovy_outfile -Force
+}
 
 echo 'executing convert_objects_to_gremlin'
 java -jar app\build\libs\app-uber.jar convert_objects_to_gremlin > $log_outfile
 
-echo '=========='
-echo 'head of groovy.txt'
-head $ddir\gremlin\groovy.txt
-
-echo '=========='
-echo 'tail of groovy.txt'
-tail $ddir\gremlin\groovy.txt
-
-echo '=========='
-echo 'see output file '$ddir\gremlin\groovy.txt
+echo 'see output file '$groovy_outfile
 
 echo 'done'
