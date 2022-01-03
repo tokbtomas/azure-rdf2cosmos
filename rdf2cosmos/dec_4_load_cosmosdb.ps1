5eea69c53b3e9086ae8d8f8450ac4d393909a3ba
Chris Joakim <christopher.joakim@gmail.com>
2022-01-03 12:13:43 -0500

    dec PowerShell scripts

--- rdf2cosmos/dec_4_load_cosmosdb.ps1
+++ rdf2cosmos/dec_4_load_cosmosdb.ps1
@@ -2,16 +2,10 @@
 # Load CosmosDB with the transformed RDF graph data.
 # Chris Joakim, Microsoft, January 2022
 
-ddir=$AZURE_RDF2COSMOS_DATA_DIR  # ddir is a shorthand abbreviation for AZURE_RDF2COSMOS_DATA_DIR
+$groovy_infile = Join-Path $env:AZURE_RDF2COSMOS_DATA_DIR '\gremlin\groovy.txt'
+$log_outfile="log\convert_objects_to_gremlin.txt"
 
-infile=$ddir"\gremlin\groovy.txt"
-log_outfile="log\load_cosmosdb_graph.txt"
-
-rm $log_outfile
-
-echo 'executing load_cosmosdb_graph'
-java -jar app\build\libs\app-uber.jar load_cosmosdb_graph $infile > $log_outfile
-
-cat $log_outfile
+echo 'executing load_cosmosdb_graph with infile: '$groovy_infile
+java -jar app\build\libs\app-uber.jar load_cosmosdb_graph $groovy_infile > $log_outfile
 
 echo 'done'
